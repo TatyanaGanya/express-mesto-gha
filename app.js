61180f0089a35c272107b6955013ce7562a2851b
TatyanaGanya <121543437+TatyanaGanya@users.noreply.github.com>
2023-08-17 23:20:53 +0300

    delete  ошибки

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use('*', (req, res) => {
-  res.status(404).send({ message: "страница не найдена"});
+  res.status(404).send({ message: 'страница не найдена' });
 });
-
 
 app.listen(PORT);
