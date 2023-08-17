258cd4df02ca85c28d030d8f1dd7039ad530123b
TatyanaGanya <121543437+TatyanaGanya@users.noreply.github.com>
2023-08-18 00:47:06 +0300

    add {new}

--- controllers/users.js
+++ controllers/users.js
@@ -44,7 +44,7 @@ module.exports.addUser = (req, res) => {
 module.exports.editUserData = (req, res) => {
   const { name, about } = req.body;
   if (req.user._id) {
-    User.findByIdAndUpdate(req.user._id, { name, about })
+    User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
       .then((user) => res.send(user))
       .catch((err) => {
         if (err.name === 'ValidationError') {
@@ -61,7 +61,7 @@ module.exports.editUserData = (req, res) => {
 // editUserAvatar,
 module.exports.editUserAvatar = (req, res) => {
   if (req.user._id) {
-    User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar })
+    User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar }, { new: true })
       .then((user) => res.send(user))
       .catch((err) => {
         if (err.name === 'ValidationError') {
